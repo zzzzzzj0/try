d86d3fb02c6da538aff7007da42cfae5ba48e4af
Spring-Lee-cpu <liyuquan3_22@163.com>
2022-03-20 16:36:00 +0800

    Update yinyong.h

--- yinyong.h
+++ yinyong.h
@@ -2,12 +2,18 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
 using namespace std;
+
 struct workpiece//工件结构
 {
-	//Point center;//重心
-	int x_min=0;
-	int x_max=0;
-	int y_min=0;
-	int y_max=0;
-	int area=0;//面积
-} Gj;
+	Point centroid;//质心
+	int zhouchang;//周长（边缘点个数）
+	Point up_left;//外接矩形左上点
+	Point dwn_rit;//外接矩形右下点
+	float m_m_ratio;//到质心最大最小距离比
+	Point near;//最近点
+	Point far;//最远点
+	vector<Point> edge;//边缘点集
+	/*下面的不知道有没有用*/
+	float k_s;//短距斜率
+	float k_l;//长距斜率
+} ;
